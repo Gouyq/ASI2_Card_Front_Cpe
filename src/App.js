4488c9d7caa46a5154bcebee878ddf2643aee92d
Gouyq <55273887+Gouyq@users.noreply.github.com>
2021-11-16 12:39:58 +0100

    persist redux

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { HeaderComponent } from './components/header/HeaderComponent';
 import { RouterApp } from './router/RouterApp';
 
 import { Provider } from 'react-redux';
-import store from './core/reducers';
+import { store, persistor } from './core/reducers';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
           <HeaderComponent></HeaderComponent>
           <RouterApp></RouterApp>
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/core/reducers/index.js
+++ src/core/reducers/index.js
@@ -1,11 +1,23 @@
 import { combineReducers, createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user.reducer'
 import cardReducer from './card.reducer'
 
-const store = createStore(combineReducers({
+const rootReducer = combineReducers({
     userState: userReducer,
     cardState: cardReducer
-}))
+})
 
-export default store
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['userState', 'cardState']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer)
+
+export const persistor = persistStore(store)
